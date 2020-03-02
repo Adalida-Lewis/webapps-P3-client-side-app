7465d66a24883cac2c5f9f011b83169760b5ee99
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-01 21:07:20 -0600

    Optimized for mobile
    Optimized for mobile using bootstrap features. Reworking of navigation bar.

--- javascript/logic.js
+++ javascript/logic.js
@@ -7,64 +7,71 @@ $(document).ready(function(){
     let finalPlayer = 0;
     let finalBot = 0;
     let control = 0;
+    let playerHold = 0;
+
     //Hides Add and Stop Buttons on load
     $("#secondSection").hide();
-
+    
+    document.getElementById("rulesButton").addEventListener("click", function(){
+        $("#rules").toggle();
+    })
+    
     document.getElementById("stopBtn").addEventListener("click",function(){
         alert("You have chose to hold at "+playerTotal);
+        playerHold++;
         $("#addBtn").hide();
         $("#stopBtn").hide();
         if(playerTotal >= botTotal){
             botTurn();
             if(botTotal > 52){
                 alert("The Bot has busted! You have won with "+ playerTotal);
+                console.log("Bot has busted");
                 finalPlayer++;
-                $("#playerWins").html(finalPlayer);  
             }
             if(botTotal <= playerTotal){
-//                alert("The bot did not make it! Their score is: " + botTotal);
                 while(botTotal < playerTotal){
-                   botTurn(); 
+                    botTurn(); 
                 }
-                
-                alert("Their Total is now: " + botTotal);
+                alert("The bots total is now: " + botTotal);
+                console.log("Updated Bot total");
             }
-            if(botTotal > playerTotal){
+            if(botTotal > playerTotal && botTotal < 52){
                 alert("The Bot is higher than you! Their score is "+botTotal);
+                console.log("Player lost to bot");
                 finalBot++;
             }
-//            if(botTotal == playerTotal){
-//                alert("It is a tie! Score is: "+botTotal);
-//                
-//            }
-
+            if(botTotal == 52 && playerTotal==52){
+                alert("It is a tie! Score is: "+botTotal);
+                console.log("Wow! Both lost because of a tie of 52!");
+            }
         }   
     })
 
     function botTurn(){
         botholder = randomGen();
-        
         if(botTotal >= 48 && botTotal <=52){
-            if(control == 0){
-                console.log("Do nothing!");
-                alert("Current number is: "+botTotal);
-                control++;
+            if(playerHold === 0){
+                if(control == 0){
+                    console.log("The bot will hold because it is close to break");
+                    alert("Bot is holding their number is: "+botTotal);
+                    control++;
+                }
             }
-            
         }
         else{
-        $("#botNumbers").append(botholder + "<br />");
-        botTotal += botholder;
-        $("#botNum").html(botTotal);
-        if(botTotal > 52){
-            alert("The bot has busted! You win with " + playerTotal);
-            playerTotal++;
-
-        }
+            $("#botNumbers").append(botholder + "<br />");
+            botTotal += botholder;
+            $("#botNum").html(botTotal);
+            if(botTotal > 52){
+                alert("The bot has busted! You win with " + playerTotal);
+                console.log("The bot has busted. Player has won");
+                playerTotal++;
             }
+        }
     }
+
     function randomGen(){
-        var randomNum = (Math.random() * 10) + 1;
+        let randomNum = (Math.random() * 10) + 1;
         return Math.round(randomNum);
     }
 
@@ -75,27 +82,27 @@ $(document).ready(function(){
         $("#currentNum").html(playerTotal);
         if (playerTotal > 52){
             alert("Busted! The bot has won with " + botTotal);
+            console.log("Player has busted");
             finalBot++;
         }
-
         if(playerTotal <= 52){
             botTurn();
         }
     })
 
     $("#submitButton").click(function(){
+        console.log("The game has started");
         let firstNumber = $("#startValue").val();
         if(firstNumber >= 1 && firstNumber <= 10){
-            
-        
-        $("#yourNumbers").append(firstNumber + "<br>");
-        playerTotal += parseInt(firstNumber);
-        $("#currentNum").html(playerTotal);
-        $("#firstSection").toggle();
-        $("#secondSection").show();
-            }
+            $("#yourNumbers").append(firstNumber + "<br>");
+            playerTotal += parseInt(firstNumber);
+            $("#currentNum").html(playerTotal);
+            $("#firstSection").toggle();
+            $("#secondSection").show();
+        }
         else{
             alert("Invalid Input! Please enter a number between 1-10");
+            console.log("The player has tried to enter a number not between 1 and 10");
         }
     });
 
