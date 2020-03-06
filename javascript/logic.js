96f90c78839f8b036f1e72542184075afbe9ef8c
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-06 13:26:14 -0600

    qUnit Attempt
    Attempting to implement qUnit

--- javascript/logic.js
+++ javascript/logic.js
@@ -2,153 +2,57 @@ $(document).ready(function(){
     //global variables
     let playerTotal = 0;
     let botTotal = 0;
-    let holder = 0;
+    let playerHolder = 0;
     let botholder = 0;
-    let finalPlayer = 0;
     let finalBot = 0;
-    let playerHold = 0;
-    let botHolding = false;
-    let playerHolding = false;
+    let endVar = 0;
+
     let sessionBotNum = localStorage.getItem('counter');
     $("#result").html(sessionBotNum);
+
     let sessionPlayerNum = localStorage.getItem('playerCounter');
     $("#playerFinal").html(sessionPlayerNum);
-
 
     //Hides Add and Stop Buttons on load
     $("#secondSection").hide();
     $("#rules").hide();
 
-    window.addEventListener('load', (event) => {
-        document.querySelector("#fname").innerHTML = localStorage.getItem('botStorageNum'); 
-        localStorage.getItem('playerStorageNum');
+    $("#submitButton").click(function(){
+        submitFirst();
     })
-    
-    
-    document.getElementById("stopBtn").addEventListener("click",function(){
-        alert("You have chose to hold at "+playerTotal);
-        playerHolding = true;
-        $("#addBtn").hide();
-        $("#stopBtn").hide();
-        if(playerTotal >= botTotal){
-            botTurn();
-            console.log("Bot hold turn");
-            if(botTotal > 52){
-                alert("The Bot has busted! You have won with "+ playerTotal);
-                console.log("Bot has busted");
-                finalPlayer++;
-                if (sessionPlayerNum === null) {
-                    sessionPlayerNum = 0;
-                } 
-                else 
-                {
-                    sessionPlayerNum++;
-                }
-                localStorage.setItem("playerCounter", sessionPlayerNum);
-
-
-
-                $("#playerFinal").html(finalBot);
-            }
-            if(botTotal <= playerTotal){
-                while(botTotal < playerTotal){
-                    botTurn(); 
-                }
-                console.log("Updated Bot total");
-            }
-            if(botTotal > playerTotal && botTotal < 52){
-                alert("The Bot is higher than you! Their score is "+botTotal);
-                console.log("Player lost to bot");
-                finalBot++;
-                $("#botFinal").html(finalBot);
-                if (sessionBotNum === null) {
-                    sessionBotNum = 0;
-                } else {
-                    sessionBotNum++;
-                }
-                localStorage.setItem("counter", sessionBotNum);
-
-
-
-            }
-            if(botTotal == 52 && playerTotal==52){
-                alert("It is a tie! Score is: "+botTotal);
-                console.log("Wow! Both lost because of a tie of 52! No one wins!");
-            }
-        }   
-    })
-
-//    function botTurn(){
-//        botholder = randomGen();
-//        if(playerHolding != false){
-//        if(botTotal >= 48 && botTotal <=52){
-//            if(botHolding != true){
-//
-//                    console.log("The bot will hold because it is close to break");
-//                    alert("Bot is holding their number is: "+botTotal);
-//                    botHolding = true;
-//                }
-//            }
-//        }
-//        else{
-//            $("#botNumbers").append(botholder + "<br />");
-//            botTotal += botholder;
-//            $("#botNum").html(botTotal);
-//            if(botTotal > 52){
-//                alert("The bot has busted! You win with " + playerTotal);
-//                console.log("The bot has busted. Player has won");
-//                playerTotal++;
-//                $("#playerFinal").html(finalBot);
-//
-//
-//
-//
-//
-//            }
-//        }       
-//    }
-    
-     function botTurn(){
-        botholder = randomGen();
-        if(botTotal >= 48 && botTotal <=52){
-            if(playerHolding != true){
-                if(botHolding == false){
-
-                    console.log("The bot will hold because it is close to break");
-                    alert("Bot is holding their number is: "+botTotal);
-                    botHolding = true;
-                    
-                }
-            }
-        }
-        else{
-            $("#botNumbers").append(botholder + "<br />");
-            botTotal += botholder;
-            $("#botNum").html(botTotal);
-            if(botTotal > 52){
-                alert("The bot has busted! You win with " + playerTotal);
-                console.log("The bot has busted. Player has won");
-                playerTotal++;
-                $("#playerFinal").html(finalBot);
-                
-                if (sessionPlayerNum === null) {
-                    sessionPlayerNum = 0;
-                } 
-                else 
-                {
-                    sessionPlayerNum++;
-                }
-                localStorage.setItem("playerCounter", sessionPlayerNum);
-            }
-        }       
-    }
 
     $("#addBtn").click(function(){
-        let holder = randomGen();
-        $("#yourNumbers").append(holder + "<br />");
-        playerTotal += holder;
+        addingNums()
+    })    
+
+    function randomGen(){
+        let randomNum = (Math.random() * 9) + 1;
+        return Math.round(randomNum);
+    }
+
+    function submitFirst(){
+        console.log("Valid starting input. The game has started.");
+        let firstNumber = $("#startValue").val();
+        if(firstNumber >= 1 && firstNumber <= 10){
+            $("#yourNumbers").append(firstNumber + "<br>");
+            playerTotal += parseInt(firstNumber);
+            $("#currentNum").html(playerTotal);
+            botTurn();
+            $("#firstSection").toggle();
+            $("#secondSection").show();
+        }
+        else{
+            console.log("You have entered an invalid number");
+            alert("Invalid Input! Please enter a number between 1 - 10");
+        }
+    }
+    
+    function addingNums(){
+        let playerHolder = randomGen();
+        $("#yourNumbers").append(playerHolder + "<br />");
+        playerTotal += playerHolder;
         $("#currentNum").html(playerTotal);
-        if (playerTotal > 52){
+        if(playerTotal >= 52){
             alert("Busted! The bot has won with " + botTotal);
             console.log("Player has busted");
             finalBot++;
@@ -158,59 +62,37 @@ $(document).ready(function(){
             } else {
                 sessionBotNum++;
             }
-            localStorage.setItem("counter", sessionBotNum);
-
-
-
-
-
-
-
-
-
-
-        }
-        if(playerTotal <= 52){
-            botTurn();
-        }
-    })
-
-    $("#submitButton").click(function(){
-        console.log("The game has started");
-        let firstNumber = $("#startValue").val();
-        if(firstNumber >= 1 && firstNumber <= 10){
-            $("#yourNumbers").append(firstNumber + "<br>");
-            playerTotal += parseInt(firstNumber);
-            $("#currentNum").html(playerTotal);
-            $("#firstSection").toggle();
-            $("#secondSection").show();
+            localStorage.setItem("counter", sessionBotNum);  
         }
         else{
-            alert("Invalid Input! Please enter a number between 1-10");
-            console.log("The player has tried to enter a number not between 1 and 10");
-        }
-    })
+            botTurn();
+        } 
 
-    function randomGen(){
-        let randomNum = (Math.random() * 9) + 1;
-        return Math.round(randomNum);
+        if(botTotal >= 52){
+            alert("The bot has lost with " + botTotal);
+            console.log("Bot has busted");
+            finalBot++;
+            $("#botFinal").html(finalBot);
+            if (sessionPlayerNum === null) {
+                sessionPlayerNum = 0;
+            } else {
+                sessionPlayerNum++;
+            }
+            localStorage.setItem("playerCounter", sessionPlayerNum);   
+        }
+    }
+    
+    function botTurn(){
+        botholder = randomGen();
+        console.log("Bot has generated a number");
+        $("#botNumbers").append(botholder + "<br />");
+        botTotal += botholder;
+        $("#botNum").html(botTotal);
     }
 
-    //    function botStorage(){
-    //        if (typeof(Storage) !== "undefined") {
-    //            // Store
-    //            localStorage.setItem("lastname", finalBot++ );
-    //            // Retrieve
-    //            document.getElementById("botFinal").innerHTML = localStorage.getItem("lastname");
-    //        } else {
-    //            document.getElementById("botFinal").innerHTML = "Sorry, your browser does not support Web Storage...";
-    //        }
-    //    }
+
+
+
 
 
 });
-
-
-
-
-
