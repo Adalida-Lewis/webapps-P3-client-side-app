2e405aecf69cb3e19d460b36c50f56a45bb81632
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-05 22:37:55 -0600

    Added ajax and session storage
    Added basic the basic features of session storage and ajax

--- javascript/logic.js
+++ javascript/logic.js
@@ -6,39 +6,50 @@ $(document).ready(function(){
     let botholder = 0;
     let finalPlayer = 0;
     let finalBot = 0;
-    let control = 0;
     let playerHold = 0;
+    let botHolding = false;
+    let playerHolding = false;
 
     //Hides Add and Stop Buttons on load
     $("#secondSection").hide();
-    
+    $("#rules").hide();
+
     document.getElementById("rulesButton").addEventListener("click", function(){
         $("#rules").toggle();
+        botStorage();
     })
-    
+
     document.getElementById("stopBtn").addEventListener("click",function(){
         alert("You have chose to hold at "+playerTotal);
         playerHold++;
         $("#addBtn").hide();
         $("#stopBtn").hide();
         if(playerTotal >= botTotal){
+
+
+
+
             botTurn();
             if(botTotal > 52){
                 alert("The Bot has busted! You have won with "+ playerTotal);
                 console.log("Bot has busted");
                 finalPlayer++;
+
+
+
+                $("#playerFinal").html(finalBot);
             }
             if(botTotal <= playerTotal){
                 while(botTotal < playerTotal){
                     botTurn(); 
                 }
-                alert("The bots total is now: " + botTotal);
                 console.log("Updated Bot total");
             }
             if(botTotal > playerTotal && botTotal < 52){
                 alert("The Bot is higher than you! Their score is "+botTotal);
                 console.log("Player lost to bot");
                 finalBot++;
+                $("#botFinal").html(finalBot);
             }
             if(botTotal == 52 && playerTotal==52){
                 alert("It is a tie! Score is: "+botTotal);
@@ -50,11 +61,12 @@ $(document).ready(function(){
     function botTurn(){
         botholder = randomGen();
         if(botTotal >= 48 && botTotal <=52){
-            if(playerHold === 0){
-                if(control == 0){
+            if(playerHold == 0){
+                if(botHolding == false){
+
                     console.log("The bot will hold because it is close to break");
                     alert("Bot is holding their number is: "+botTotal);
-                    control++;
+                    botHolding = true;
                 }
             }
         }
@@ -66,13 +78,9 @@ $(document).ready(function(){
                 alert("The bot has busted! You win with " + playerTotal);
                 console.log("The bot has busted. Player has won");
                 playerTotal++;
+                $("#playerFinal").html(finalBot);
             }
-        }
-    }
-
-    function randomGen(){
-        let randomNum = (Math.random() * 10) + 1;
-        return Math.round(randomNum);
+        }       
     }
 
     $("#addBtn").click(function(){
@@ -84,6 +92,7 @@ $(document).ready(function(){
             alert("Busted! The bot has won with " + botTotal);
             console.log("Player has busted");
             finalBot++;
+            $("#botFinal").html(finalBot);
         }
         if(playerTotal <= 52){
             botTurn();
@@ -104,7 +113,28 @@ $(document).ready(function(){
             alert("Invalid Input! Please enter a number between 1-10");
             console.log("The player has tried to enter a number not between 1 and 10");
         }
-    });
+    })
 
-})
+    function randomGen(){
+        let randomNum = (Math.random() * 9) + 1;
+        return Math.round(randomNum);
+    }
+
+    function botStorage(){
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            localStorage.setItem("lastname", finalBot++ );
+            // Retrieve
+            document.getElementById("botFinal").innerHTML = localStorage.getItem("lastname");
+        } else {
+            document.getElementById("botFinal").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+    }
+
+
+});
+
+
+
+
 
